32b8e9ae742af08ffcebd4041d5b071df1cd7072
Ravindranath <ravindranathbe@gmail.com>
2016-12-28 19:05:00 +0530

    list notes from server

--- notesapp/main.js
+++ notesapp/main.js
@@ -1,9 +1,24 @@
 import Vue from 'vue';
 import App from './components/App.vue';
+import { store } from './vuex/store'
+
+Vue.use(require('vue-resource'));
 
 var notesapp = new Vue({
   	el: '#app',
-  	components: { App }
+  	components: { App },
+  	mounted: function() {
+  		this.getNotes()
+  	},
+  	methods: {
+  		getNotes() {
+  			this.$http.get('notesBackend/public/notes').then((response) => {
+	        store.state.notes = JSON.parse(response.body)
+	      }, (response) => {
+        	console.log(response);
+      	});
+  		}
+  	}
 })
 
 window.notesapp = notesapp;
